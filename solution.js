ae17e55c76bba58e30a721f6ef08fa11698138b2
Narawit Sawatdecha <nsawatdecha@myseneca.ca>
2025-03-29 00:22:35 -0400

    Q1-2 test

---
+++ solution.js
@@ -0,0 +1,6 @@
+//Question 2 DOMContentLoaded 
+document.title = 'WEB222 Winter 2025 Test 5 - Your Name';
+//Question 1 DOMContentLoaded 
+document.addEventListener('DOMContentLoaded', function (){
+
+}
